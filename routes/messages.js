5f607731b66a5d582e4a52f92ab68f00c1430a25
dongatuna <ngatuna05@gmail.com>
2019-02-25 06:13:00 -0800

    copilot working

--- routes/messages.js
+++ routes/messages.js
@@ -1,26 +1,27 @@
 //const express = require('express')
 //const router = express.Router()
-const TWILIO_CONFIG = require('../config')
-const accountSid = TWILIO_CONFIG.account_SID
-const authToken = TWILIO_CONFIG.auth_Token
-const twilio = require('twilio')
-const client = new twilio(accountSid, authToken)
+//const TWILIO_CONFIG = require('../config')
+const accountSid = 'ACb11e421fcca270377a9c1f08bec23c40'
+const authToken ='48301b22e70eda86ac44c5a3207f87d6'
+//const twilio = require('twilio')
+const client = require('twilio')(accountSid, authToken)
 
 
 //router.post('https://api.twilio.com/2010-04-01/Accounts/'+accountSid+'/Messages', (req, res, next)=>{})
 
-const time = new Date().toLocaleTimeString().split(" ")[0]
+//const time = new Date().toLocaleTimeString().split(" ")[0]
 
 //sends a message from my twilio number to a cell phone
 client.messages
     .create(
         {
-            body: `Greetings! The current time is: ${time} L6RCYQT46S8BUED`,
-            messagingServicecSid:'MGe32e38f35a896ea2029289bc77878f1a',            
+            messagingServiceSid:'MGe32e38f35a896ea2029289bc77878f1a',  
+            body: `Greetings! The current time is...`,                     
             to: `+12062711946`
         }
     ).then(message => console.log(message.sid))
-    .catch(error => console.log(error))
+    .catch(error => console.log(`Error:`, error))
+
 
 
 //get the local time
